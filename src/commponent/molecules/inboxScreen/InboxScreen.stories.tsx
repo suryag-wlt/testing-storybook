017b46ec6d90d1d4c94ebddab2f408ca0663ebe2
surya.wlt <suryag.wlt@gmail.com>
2025-08-28 12:35:55 +0530

    Add all project files

--- src/commponent/molecules/inboxScreen/InboxScreen.stories.tsx
+++ src/commponent/molecules/inboxScreen/InboxScreen.stories.tsx
@@ -1,10 +1,7 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite';
-
 import InboxScreen from './InboxScreen';
-
 import store from '../../../lib/store';
-
 import { Provider } from 'react-redux';
 
 const meta = {
